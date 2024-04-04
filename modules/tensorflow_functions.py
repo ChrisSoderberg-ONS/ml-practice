ea04753aa23dfe473b9f02c2c0218b209ae8c4ac
soderc <christoffer.soderberg@ons.gov.uk>
2024-04-04 17:58:57 +0100

    Added section on data partitioning

--- modules/tensorflow_functions.py
+++ modules/tensorflow_functions.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import pandas as pd
 from matplotlib import pyplot as plt
-import numpy
+import numpy as np
 
 def build_lr_model(learning_rate):
     """
@@ -37,16 +37,44 @@ def train_model(model, df, feature, label, epochs, batch_size):
         batch_size = batch_size
     )
 
+    # Gather the model's trained weight and bias
     trained_weight = model.get_weights()[0]
     trained_bias = model.get_weights()[1]
 
+    # The list of epochs is stored separately from the rest of history
     epochs = history.epoch
 
+    # Isolate the root mean squared error for each epoch
     hist = pd.DataFrame(history.history)
-
     rmse = hist["root_mean_squared_error"]
 
     return trained_weight, trained_bias, epochs, rmse
+
+
+def train_model_with_validation(model, df, feature, label, epochs, batch_size, validation_split):
+    """
+    Train model by feeding data. Includes extra parameter to partition training set into validation set
+    """
+    history = model.fit(
+        x = df[feature],
+        y = df[label],
+        epochs = epochs,
+        batch_size = batch_size,
+        validation_split = validation_split
+    )
+
+    # Gather the model's trained weight and bias
+    trained_weight = model.get_weights()[0]
+    trained_bias = model.get_weights()[1]
+
+    # The list of epochs is stored separately from the rest of history
+    epochs = history.epoch
+
+    # Isolate the root mean squared error for each epoch
+    hist = pd.DataFrame(history.history)
+    rmse = hist["root_mean_squared_error"]
+
+    return trained_weight, trained_bias, epochs, rmse, history.history
 
 
 def predict_from_random(model, df, feature, n):
@@ -54,7 +82,7 @@ def predict_from_random(model, df, feature, n):
     Predict label from random feature data
     """
 
-    batch = numpy.random.uniform(
+    batch = np.random.uniform(
         df[feature].quantile(.1), 
         df[feature].quantile(.9), 
         size=n)
@@ -111,4 +139,30 @@ def plot_the_loss_curve(epochs, rmse):
     plt.plot(epochs, rmse, label="Loss")
     plt.legend()
     plt.ylim([rmse.min()*0.97, rmse.max()])
+    plt.show()
+
+
+def plot_the_loss_curve_with_validation(epochs, mae_training, mae_validation):
+    """
+    Plot the loss curve for the training set and the validation set
+    """
+
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Root Mean Squared Error")
+
+    plt.plot(epochs[1:], mae_training[1:], label="Training Loss")
+    plt.plot(epochs[1:], mae_validation[1:], label="Validation Loss")
+    plt.legend()
+
+    merged_mae_lists = mae_training[1:] + mae_validation[1:]
+    highest_loss = max(merged_mae_lists)
+    lowest_loss = min(merged_mae_lists)
+    delta = highest_loss - lowest_loss
+    print(delta)
+
+    top_of_y_axis = highest_loss + (delta * 0.05)
+    bottom_of_y_axis = lowest_loss - (delta * 0.05)
+   
+    plt.ylim([bottom_of_y_axis, top_of_y_axis])
     plt.show()
